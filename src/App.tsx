17a2eaf8b8fd72f7a3d3156d158169e5149d5212
Radexman <Borderlandsmaniak@gmail.com>
2024-06-05 15:47:21 +0200

    Redux Basic Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Counter from "./features/counter/Counter";
+
 const App = () => {
   return (
     <main className="App">
-      <h1>Redux toolkit course</h1>
+      <Counter />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,10 +1,74 @@
+import type { ChangeEvent } from "react";
+import { useState } from "react";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
-import { increment, decrement } from "./counterSlice";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+  decrementByAmount,
+} from "./counterSlice";
 
 const Counter = () => {
+  const [number, setNumber] = useState(0);
   const { count } = useAppSelector(state => state.counter);
+  const dispatch = useAppDispatch();
 
-  return <div>Counter</div>;
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
+  const handleChangeNumber = (e: ChangeEvent<HTMLInputElement>) => {
+    const correctNumber = Number(e.target.value);
+    setNumber(correctNumber);
+  };
+
+  const handleIncrementByAmount = () => {
+    dispatch(incrementByAmount(number));
+  };
+
+  const handleDecrementByAmount = () => {
+    dispatch(decrementByAmount(number));
+  };
+
+  return (
+    <section>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={handleIncrement} type="button">
+          +
+        </button>
+        <button onClick={handleDecrement} type="button">
+          -
+        </button>
+      </div>
+      <form>
+        <p>Select a number from 1 - 10 and increment / decrement the count</p>
+        <label htmlFor="number">
+          <input
+            type="number"
+            name="number"
+            id="number"
+            min={1}
+            max={10}
+            value={number}
+            onChange={handleChangeNumber}
+          />
+        </label>
+        <div className="button-container">
+          <button onClick={handleIncrementByAmount} type="button">
+            Add Amount
+          </button>
+          <button onClick={handleDecrementByAmount} type="button">
+            Remove Amount
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default Counter;
